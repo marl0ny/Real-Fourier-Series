ad870b840ce7b153a5afd1cc56f6d06f8a88eac3
marl0ny <52328879+marl0ny@users.noreply.github.com>
2020-08-13 20:46:37 -0400

    Modified the qt sliders and added a license
    The sliders in the qt version can be changed by double clicking on its label and then changing its value through text entry.

--- qt_widgets/__init__.py
+++ qt_widgets/__init__.py
@@ -39,6 +39,15 @@ class Slider(QtWidgets.QSlider):
         """
         self._observers = slider_observers
 
+    def add_observers(self, slider_observer: QtWidgets.QWidget) -> None:
+        """
+        Add a slider observer.
+
+        Parameters:
+         slider_observer: an observer.
+        """
+        self._observers.append(slider_observer)
+
     def set_number_of_ticks(self, number_of_ticks: int) -> None:
         """
         Set the total number of intervals in the slider.
@@ -103,6 +112,124 @@ class Slider(QtWidgets.QSlider):
         return {'value': val, 'id': self._slider_id}
 
 
+class LabelWithLineEdit(QtWidgets.QWidget):
+    """
+    Label with line edit next to it.
+    """
+    def __init__(self, label: str = "", 
+                 parent: QtWidgets.QWidget = None) -> None:
+        """
+        Constructor
+
+        Parameters:
+         label: set the label for this widget
+         parent: the parent of this widget
+        """
+        QtWidgets.QWidget.__init__(self)
+        if not parent:
+            parent = self
+        self.layout = QtWidgets.QHBoxLayout(self)
+        self.layout.setMargin(0)
+        self._label = QtWidgets.QLabel(label)
+        self._line_edit = QtWidgets.QLineEdit()
+        self._line_edit.returnPressed.connect(parent.line_edit_returned)
+        self.layout.addWidget(self._label)
+        self.layout.addWidget(self._line_edit)
+
+    def set_line_edit_label(self, text: str) -> None:
+        """
+        When the text on this widget is being edited,
+        set the label for the line edit.
+
+        Parameters:
+         text: the text to set the line edit label.
+        """
+        self._label.setText(text)
+
+    def set_line_edit(self, text: str) -> None:
+        """
+        Set the text shown on the line edit.
+
+        Parameters:
+         text: the text for the line edit itself.
+        """
+        self._line_edit.setText(text)
+
+    def text(self) -> str:
+        """
+        Getter for the text label.
+        
+        Returns:
+         text label.
+        """
+        return self._line_edit.text()
+
+
+class EditableLabel(QtWidgets.QStackedWidget):
+    """
+    An editable label.
+    """
+    def __init__(self, label: str = "", 
+                 parent: QtWidgets.QWidget = None) -> None:
+        """
+        Constructor
+
+        Parameters:
+         label: set the label for this widget
+         parent: the parent of this widget
+        """
+        QtWidgets.QStackedWidget.__init__(self)
+        self._parent = parent
+        self._label = QtWidgets.QLabel(label)
+        self._line_edit = LabelWithLineEdit(parent=self)
+        self.addWidget(self._label)
+        self.addWidget(self._line_edit)
+
+    def setText(self, text: str) -> None:
+        """
+        Set the text of the label when the widget
+        is not being edited.
+
+        Parameters:
+         text: the text on the label.
+        """
+        self._label.setText(text)
+
+    def set_line_edit_label(self, text: str) -> None:
+        """
+        When the text on this widget is being edited,
+        set the label for the line edit.
+
+        Parameters:
+         text: the text to set the line edit label.
+        """
+        self._line_edit.set_line_edit_label(text)
+
+    def set_line_edit(self, text: str) -> None:
+        """
+        Set the text shown on the line edit.
+
+        Parameters:
+         text: the text for the line edit itself.
+        """
+        self._line_edit.set_line_edit(text)
+
+    def line_edit_returned(self) -> None:
+        """
+        When the return key is pressed on the line edit
+        enter the value to the slider and only display the label.
+        """
+        # if self.currentIndex() == 1 and self._parent:
+        self.setCurrentIndex(not self.currentIndex())
+        self._parent.set_slider(float(self._line_edit.text()))
+
+    def mouseDoubleClickEvent(self, 
+                              qt_event: QtGui.QMouseEvent) -> None:
+        self.setCurrentIndex(not self.currentIndex())
+        if self.currentIndex() == 0 and self._parent:
+            self._parent.set_slider(float(self._line_edit.text()))
+
+
 class HorizontalSliderBox(QtWidgets.QGroupBox):
     """
     GUI Box containing a slider as well as some other widgets.
@@ -120,9 +247,11 @@ class HorizontalSliderBox(QtWidgets.QGroupBox):
         self.setMinimumWidth(250)
         self.setMaximumWidth(300)
         self.setMaximumHeight(100)
-        self._string_format = "%s = %.2f"
+        self._varname_equals_string = "%s ="
+        self._string_format = self._varname_equals_string + " %.2f"
+        self._number_format = "%.2f"
         self._layout = QtWidgets.QVBoxLayout()
-        self._label = QtWidgets.QLabel("Set " + str(slider_id))
+        self._label = EditableLabel("Set " + str(slider_id), parent=self)
         self._slider = Slider(slider_id,
                               QtCore.Qt.Horizontal,
                               context)
@@ -177,7 +306,9 @@ class HorizontalSliderBox(QtWidgets.QGroupBox):
          string format: the string format to display the value
          of the slider.
         """
-        self._string_format = "%s = " + string_format
+        self._number_format = string_format
+        self._string_format = self._varname_equals_string + \
+                              ' ' + string_format
 
     def on_slider_changed(self, slider_input: dict) -> None:
         """
@@ -188,6 +319,10 @@ class HorizontalSliderBox(QtWidgets.QGroupBox):
         """
         val = slider_input['value']
         slider_id = slider_input['id']
+        self._label.set_line_edit_label(self._varname_equals_string
+                                        % slider_id)
+        self._label.setCurrentIndex(0)
+        self._label.set_line_edit(self._number_format % val)
         self._label.setText(self._string_format % (slider_id, val))
 
     def destroy_slider(self) -> None:
